aeb00984f9105d9902119d21cd3ee9b0abd53a51
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-01 14:55:32 +0100

    Modif

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for (let i = 0; i < classes.length; i++) {
     if ((i % 2) === 0) {
         continue;
     }
-    if (i === (classes.length - 1)) {
+    else if (i === (classes.length - 1)) {
         break
     }
     let classe = classes[i];
